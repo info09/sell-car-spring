db5b79041ca719d24135e08916348e7b140ffb01
huytq@ics-p.vn <huytq@ics-p.vn>
2024-10-02 10:45:54 +0700

    Save JWT In Local Storage

--- sellcar_angular/src/app/auth/auth-components/login/login.component.ts
+++ sellcar_angular/src/app/auth/auth-components/login/login.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth/auth.service';
+import { NzMessageComponent, NzMessageService } from 'ng-zorro-antd/message';
+import { Router } from '@angular/router';
+import { StorageService } from '../../services/storage/storage.service';
 
 @Component({
   selector: 'app-login',
@@ -11,7 +14,12 @@ export class LoginComponent {
   loginForm: FormGroup;
   isSpinning: boolean = false;
 
-  constructor(private fb: FormBuilder, private authService: AuthService) {
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private message: NzMessageService,
+    private router: Router
+  ) {
     this.loginForm = this.fb.group({
       email: [null, Validators.required],
       password: [null, Validators.required],
@@ -22,6 +30,21 @@ export class LoginComponent {
     console.log(this.loginForm.value);
     this.authService.login(this.loginForm.value).subscribe((res) => {
       console.log(res);
+      if (res.userId != null) {
+        const user = {
+          id: res.userId,
+          role: res.userRole,
+        };
+        StorageService.saveUser(user);
+        StorageService.saveToken(res.jwt);
+        if (StorageService.isAdminLoggedIn()) {
+          this.router.navigateByUrl('/admin/dashboard');
+        } else {
+          this.router.navigateByUrl('/customer/dashboard');
+        }
+      } else {
+        this.message.error('Bad credentials', { nzDuration: 5000 });
+      }
     });
   }
 }
