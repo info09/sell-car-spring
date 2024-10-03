bef4f4e2cd3942cdd2e9200595e3fe91366bfc67
huytq <huytq3103@gmail.com>
2024-10-03 21:29:48 +0700

    add Car entity, CarDTO, and CarRepository

---
+++ Sell_Car/src/main/java/com/coding/dto/CarDTO.java
@@ -0,0 +1,25 @@
+package com.coding.dto;
+
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Date;
+
+@Data
+@Builder
+public class CarDTO {
+    private Long id;
+    private String name;
+    private String brand;
+    private String type;
+    private String transmission;
+    private String color;
+    private Date year;
+    private Boolean sold;
+    private String description;
+    private MultipartFile img;
+    private Long userId;
+    private Long price;
+    private byte[] returnedImg;
+}
