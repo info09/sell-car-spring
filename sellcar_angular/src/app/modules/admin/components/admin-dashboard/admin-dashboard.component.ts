d18ee62c19bc47c2ed79d929d447669216a49f28
huytq@ics-p.vn <huytq@ics-p.vn>
2024-10-07 10:50:37 +0700

    get all car for admin

--- sellcar_angular/src/app/modules/admin/components/admin-dashboard/admin-dashboard.component.ts
+++ sellcar_angular/src/app/modules/admin/components/admin-dashboard/admin-dashboard.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AdminService } from '../../services/admin.service';
 
 @Component({
   selector: 'app-admin-dashboard',
   templateUrl: './admin-dashboard.component.html',
-  styleUrl: './admin-dashboard.component.scss'
+  styleUrl: './admin-dashboard.component.scss',
 })
-export class AdminDashboardComponent {
+export class AdminDashboardComponent implements OnInit {
+  cars: any = [];
+  constructor(private adminService: AdminService) {}
+  ngOnInit(): void {
+    this.getCar();
+  }
 
+  getCar() {
+    this.adminService.getAllCars().subscribe((result) => {
+      this.cars = result;
+    });
+  }
 }
--- sellcar_angular/src/app/modules/admin/services/admin.service.ts
+++ sellcar_angular/src/app/modules/admin/services/admin.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { Observable } from 'rxjs';
+import { StorageService } from '../../../auth/services/storage/storage.service';
+const BASE_URL = 'http://localhost:8080/';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AdminService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getAllCars(): Observable<any> {
+    return this.http.get(BASE_URL + 'api/admin/cars', {
+      headers: this.createAuthorizationHeader(),
+    });
+  }
+
+  createAuthorizationHeader(): HttpHeaders {
+    let authHeaders: HttpHeaders = new HttpHeaders();
+    return authHeaders.set(
+      'Authorization',
+      `Bearer ${StorageService.getToken()}`
+    );
+  }
 }
